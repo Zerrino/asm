e3b168807f30ac0d88af38d7cf5d38e97575fa8f
Alexandre Ferreira Da Silva <alexafer@shi-r11-p3.s19.be>
2024-04-18 19:11:33 +0200

    eh

--- libASM/ft_strcmp.s
+++ libASM/ft_strcmp.s
@@ -4,11 +4,20 @@ section	.text
 ;	rdi str : char *  rsi char *
 
 _ft_strcmp:
-		mov		rax, 0
-		
+		mov		al, BYTE [rdi]
+		mov		bl, BYTE [rsi]
+		cmp		al, 0
+		je		ft_finish
+		cmp		bl, 0
+		je		ft_finish
+		cmp		al, bl
+		jne		ft_finish
+		inc		rdi
+		inc		rsi
+		jmp		_ft_strcmp
 
-
-
-
+ft_finish:
+		movzx	rax, al
+		movzx	rbx, bl
+		sub		rax, rbx
 		ret
-
--- libASM/main.c
+++ libASM/main.c
@@ -6,7 +6,7 @@
 /*   By: alexafer <alexafer@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 09:59:28 by alexafer          #+#    #+#             */
-/*   Updated: 2024/04/18 18:48:04 by alexafer         ###   ########.fr       */
+/*   Updated: 2024/04/18 19:11:22 by alexafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int  main(int argc, char **argv)
 {
 	(void)argv;
 	(void)argc;
-	printf("ft_strcmp : %d\n", ft_strcmp(argv[1], argv[2]));
-	printf("strcmp : %d\n", strcmp(argv[1], argv[2]));
+	//printf("ft_strcmp : %d\n", ft_strcmp(argv[1], argv[2]));
+	//printf("strcmp : %d\n", strcmp(argv[1], argv[2]));
  	return (0);
 }
