f834047557ffbdc9ad2fb54e0e247e95a907773d
Alexandre Ferreira Da Silva <alexafer@shi-r11-p3.s19.be>
2024-04-19 18:32:22 +0200

    update

--- libASM/ft_atoi_base.s
+++ libASM/ft_atoi_base.s
@@ -32,6 +32,16 @@ ft_base_spec:
 		je		ft_base_error
 		cmp		BYTE [rdi + rax], ' '
 		je		ft_base_error
+		cmp		BYTE [rdi + rax], 9
+		je		ft_base_error
+		cmp		BYTE [rdi + rax], 10
+		je		ft_base_error
+		cmp		BYTE [rdi + rax], 11
+		je		ft_base_error
+		cmp		BYTE [rdi + rax], 12
+		je		ft_base_error
+		cmp		BYTE [rdi + rax], 13
+		je		ft_base_error
 		dec		rax
 		jmp		ft_base_spec
 
@@ -40,7 +50,7 @@ ft_same_id:
 		jmp		ft_end_char
 
 ft_twice_char: ; Donc on a R10 et RAX qui sont = ft_strlen donc la len
-		call	_ft_strlen
+		mov		rax, r11
 		cmp		r10, -1
 		je		ft_no_err
 ft_loop_char:
@@ -62,6 +72,7 @@ _ft_atoi_base:
 		mov		r9, rdi			; R9 -> char *NB
 		mov		rdi, rsi		; RDI -> char *BASE
 		call	_ft_strlen		;
+		mov		r11, rax		; storer la long dans r11
 		cmp		rax, 2			;	
 		js		ft_base_error 	; SI len base < 2 go return 0
 		jmp		ft_base_spec	; CHECK presence de '+' '-' ou ' '
@@ -71,6 +82,76 @@ ft_next:
 		mov		r10, rax		; On me 0 dans R10 pour faire une double boucle while
 		jmp	ft_twice_char
 
-ft_no_err:
-		; Donc on arrive ici Sans erreur !
+_ft_is_inside:	; regarde si char est dans char *str, si oui return l'index dans str
+				; rdi = char, rsi char *, rdx = len
+				; return danx rax
+				;
+		cmp		rdx, -1
+		je		ft_inside
+		cmp		dil, [rsi + rdx]
+		je		ft_inside
+		dec		rdx
+		jmp		_ft_is_inside
+
+
+ft_inside:
+		mov		rax, rdx
 		ret
+
+ft_s_error:
+		mov		rax, -1
+		ret
+
+_ft_start: ; rdi char *nb, rsi char *base, rdx len, r8b
+		mov		r10, 0
+ft_s_loop:
+		cmp		rdx, r10
+		je		ft_s_end
+		mov		r8b, [rdi + r10]
+		cmp		r8b, ' '
+		je		ft_s_inc
+		cmp		r8b, 9
+		je		ft_s_inc
+		cmp		r8b, 10
+		je		ft_s_inc
+		cmp		r8b, 11
+		je		ft_s_inc
+		cmp		r8b, 12
+		je		ft_s_inc
+		cmp		r8b, 13
+		je		ft_s_inc
+		cmp		r8b, '+'
+		je		ft_s_end
+		cmp		r8b, '-'
+		je		ft_s_end
+		mov		r12, rdi
+		mov		r13, rdx
+		mov		rdx, r11
+		mov		dil, r8b
+		call	_ft_is_inside
+		mov		rdi, r12
+		mov		rdx, r13
+		cmp		rax, 0
+		jns		ft_s_end
+		jmp		ft_s_error
+
+ft_s_inc:
+		inc		r10
+		jmp		ft_s_loop	
+
+ft_s_end:
+		mov		rax, r10
+		ret
+
+ft_no_err:
+		; Donc on arrive ici Sans erreur ! avec R9 -> CHAR *NB, RDI -> *BASE R11 = len base
+		mov		rsi, rdi ; on deplace *BASE dans RSI
+		mov		rdx, r11 ; la len dans rdx
+		mov		rdi, r9
+		call	_ft_strlen
+		mov		rdx, rax
+		call	_ft_start
+		cmp		rax, -1
+		je		ft_base_error
+		ret
+
--- libASM/main.c
+++ libASM/main.c
@@ -6,7 +6,7 @@
 /*   By: alexafer <alexafer@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 09:59:28 by alexafer          #+#    #+#             */
-/*   Updated: 2024/04/19 14:53:48 by alexafer         ###   ########.fr       */
+/*   Updated: 2024/04/19 18:32:08 by alexafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,22 @@
 
 int  main(int argc, char **argv)
 {
+	char	*str;
 
-	if (argc < 2)
-		return (0);
-	printf("Nb : %d\n", ft_atoi_base(argv[1], argv[2]));
+
+	(void)argc;
+	(void)argv;
+	str = (char *)ft_malloc(sizeof(char) * 100);
+	
+	str[0] = 'h';
+	str[1] = 'e';
+	str[2] = 'y';
+	str[3] = '\n';
+	str[4] = '\0';
+
+	ft_write(1, str, ft_strlen(str));
+	//free(str);
+	ft_write(1, str, ft_strlen(str));
+	system("leaks a.out");
 	return (0);
 }
