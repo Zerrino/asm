3109b6c54166df0097d5042c16c4dabf979278ba
alexafer <alexafer@MacBook-Air-de-alexafer.local>
2024-05-07 07:02:25 +0200

    commentless

--- LIBA/ft_list_remove_if.s
+++ LIBA/ft_list_remove_if.s
@@ -2,36 +2,69 @@
 section	.text
 	global _ft_list_remove_if
 	extern _ft_list_size
-
+	extern _free
 ; ft_list_remove_if (t_list **, void *data_ref, int (*cmp)(), (*free_fct)(void *))
 ;					rdi, rsi, rdx, rcx
+
+%macro	S_ARGS 0
+		push			rdi ; [rsp + 24]  t_list **
+		push			rsi ; [rsp + 16]  data_ref
+		push			rdx ; [rsp + 8]   int cmp
+		push			rcx ; [rsp]       free_fct
+%endmacro
+
+%macro	R_ARGS 0
+		pop				rcx
+		pop				rdx
+		pop				rsi
+		pop				rdi
+%endmacro
+
 _ft_list_remove_if:
-		mov			r15, rsi ; data_ref
-		mov			r14, rdx ; (*cmp)()
-		mov			r13, r10 ; ft_remove
-		mov			r12, rdi ; t_list **
-		mov			rdi, [rdi]
-		call		_ft_list_size
-		mov			r11, rax; list_size var baby?!
-		mov			r12, [r12]
-ft_start:
-		cmp			r12, 0
-		je			ft_ret
-		
-		mov			rdi, [r12]
-		mov			rsi, r15
-		call		r14
-		je			ft_rem
-ft_aft:
-		mov			r12, [r12 + 8]
-		dec			r11
-		jmp			ft_start
+		S_ARGS
+		cmp				[rsp + 24], word 0
+		je				ft_ret
+		cmp				[rsp + 8], word 0
+		je				ft_ret
+		cmp				[rsp], word 0
+		je				ft_ret
+		mov				r8, [rdi]
+		mov				r9, 0
+ft_loop:
+		cmp				r8, 0
+		je				ft_ret	
+		mov				rdi, [r8]
+		mov				rsi, [rsp + 16]
+		call			[rsp + 8]
+		cmp				rax, 0
+		je				ft_remove
+		mov				r9, r8
+		mov				r8, [r8 + 8]
+		jmp				ft_loop
+
+
+ft_remove:
+		cmp				r9, 0
+		je				ft_no_p
+		mov				r10, [r8 + 8]
+		mov				[r9 + 8], r10
+		jmp				ft_free_data
+ft_no_p:
+		mov				r10, [r8 + 8]
+		mov				r11, [rsp + 24]
+		mov				[r11], r10
+		jmp				ft_free_data
+
+ft_free_data:
+		mov				rdi, [r8]
+		call			[rsp + 16]
+		mov				rdi, r8
+		mov				r8, [r8 + 8]
+		sub				rsp, 8
+		call			_free
+		add				rsp, 8
+		jmp				ft_loop
 
 ft_ret:
-		mov			rax, r11
-		ret
-ft_rem:
-		mov			rdi, [r12]
-		call		rcx
-		jmp			ft_aft
+		R_ARGS
 		ret
